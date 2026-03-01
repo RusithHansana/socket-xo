434129dca93d86c4b6e8dec6e20adab8a24f3028
rusithhansana <rusithhansana.dev@gmail.com>
2026-03-01 14:42:39 +0530

    Refactor package.json: format workspaces array and update devDependencies

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ import tseslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import reactHooks from 'eslint-plugin-react-hooks';
 import prettier from 'eslint-config-prettier';
+import reactPlugin from 'eslint-plugin-react';
+import globals from 'globals';
 
 export default [
   {
@@ -16,16 +18,28 @@ export default [
         ecmaVersion: 'latest',
         sourceType: 'module',
       },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
     plugins: {
       '@typescript-eslint': tseslint,
       'react-hooks': reactHooks,
+      react: reactPlugin,
     },
     rules: {
       ...tseslint.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...reactPlugin.configs['jsx-runtime'].rules,
       '@typescript-eslint/no-explicit-any': 'error',
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
   // Prettier compatibility (disables conflicting rules)
