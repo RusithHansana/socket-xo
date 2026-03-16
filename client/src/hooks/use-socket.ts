5946b0540f35d68ea220171c09428da659be4551
RusithHansana <97432521+RusithHansana@users.noreply.github.com>
2026-03-16 15:39:54 +0530

    feat: expose socket on the global window object for testing purposes

--- client/src/hooks/use-socket.ts
+++ client/src/hooks/use-socket.ts
@@ -2,13 +2,19 @@ import { useContext } from 'react';
 import { SocketContext } from '../contexts/socket.context';
 import type { TypedSocket } from '../services/socket-service';
 
+declare global {
+  interface Window {
+    socket?: TypedSocket | null;
+  }
+}
+
 export function useSocket(): TypedSocket | null {
   const socket = useContext(SocketContext);
   if (socket === undefined) {
     throw new Error('useSocket must be used within a SocketProvider');
   }
-  //TODO: Remove in production - only for testing purposes
-  (window as any).socket = socket; // Expose the socket for testing purposes
+  // TODO: Remove in production - only for testing purposes.
+  window.socket = socket;
 
   return socket;
 }
